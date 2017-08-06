625ee82864b7d7e7292e3eec4ce4871278ae7267
Felipe Brum <fbrump@gmail.com>
2017-08-06 17:24:45 -0300

    Adjusted Conta.cs and create new classes for keep implementations for Filtro (filter).

--- src/CursoDesignPatterns/Conta.cs
+++ src/CursoDesignPatterns/Conta.cs
@@ -1,11 +1,22 @@
 namespace CursoDesignPatterns
 {
+    using System;
+
     public class Conta
     {
+        public Conta()
+        {
+            this.DataAbertura = DateTime.Now;
+        }
+
         public double Saldo {get; private set; }
+
+        public DateTime DataAbertura { get; private set;}
+
         public void Deposita(double valor)
         {
             this.Saldo += valor;
         }
+
     }
 }
