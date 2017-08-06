f3df067ae863f820e2607a900897c3e78720a6ad
Felipe Brum <fbrump@gmail.com>
2017-08-06 20:39:13 -0300

    Created two class, one for save on DataBase and other to send into SMS.

---
+++ src/CursoDesignPatterns/Observer/EnviadorDeSms.cs
@@ -0,0 +1,14 @@
+namespace CursoDesignPatterns.Observer
+{
+    using System;
+    using Venda;
+    
+    public class EnviadorDeSms
+    {
+        private void enviaPorSms(NotaFiscal nf)
+        {
+            Console.WriteLine("Simula - Envia por SMS.");
+        }
+
+    }
+}
---
+++ src/CursoDesignPatterns/Observer/NotaFiscalDao.cs
@@ -0,0 +1,13 @@
+namespace CursoDesignPatterns.Observer
+{
+    using System;
+    using Venda;
+
+    public class NotaFiscalDao
+    {
+        public void SalvaNoBanco(NotaFiscal nf)
+        {
+            Console.WriteLine("Simula - Salva no banco.");
+        }
+    }
+}
